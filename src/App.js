83620423ad25c54e7208e9c209687ace2a451e3e
stewart <949257156@qq.com>
2024-07-30 16:40:00 +0800

    user

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
-import "./App.css";
-import NavBar from "./components/admin/NavBar.js";
-import CategoriesView from "./components/admin/View.js";
-import Home from "./components/admin/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Add from "./components/admin/Add";
-import Edit from "./components/admin/Edit.js";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Layout } from 'antd';
+import Home from './Home';
+import BudgetSet from './components/user/BudgetSet';
+import NavBar from './components/user/NavBar';
 
-function App() {
-  return (
-    <main className="container mt-5">
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/view-categories" element={<CategoriesView />} />
-          <Route exact path="/add-category" element={<Add />} />
-          <Route exact path="/edit-category/:id" element={<Edit />} />
-        </Routes>
-      </Router>
-    </main>
+const { Header, Content } = Layout;
+
+const App = () => {
+  return ( <Router>
+      <Layout>
+        <Header>
+          <NavBar />
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <div className="site-layout-content" style={{ margin: '16px 0' }}>
+            <Routes>
+              <Route path="/dashboard" element={<Home />} />
+              <Route path="/budgetset" element={<BudgetSet />} />
+              <Route path="/logout" element={<div>Logout</div>} />
+            </Routes>
+          </div>
+        </Content>
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
