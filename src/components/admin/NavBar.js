18605cca7cf23fd63a5b65b8e7b14b493a43b5c4
XXXZZZHHH123 <e1133135@u.nus.edu>
2024-08-05 18:41:15 +0800

    fix admin navbar problem

--- src/components/admin/NavBar.js
+++ src/components/admin/NavBar.js
@@ -22,8 +22,8 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-5 w-100">
       <div className="container-fluid">
-        <Link className="navbar-brand" to={"/"}>
-          SmartBook
+        <Link className="navbar-brand" to={"/admin"}>
+          Admin
         </Link>
         <button
           className="navbar-toggler"
@@ -39,18 +39,56 @@ const NavBar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100">
             <li className="nav-item dropdown">
-              <Link className="nav-link" to={"/user/dashboard"}>
-                Dashboard
+              <Link
+                className="nav-link dropdown-toggle"
+                to="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                View Categories
+              </Link>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <Link
+                    className="dropdown-item"
+                    to={"/admin/view-categories_all"}
+                  >
+                    Categories (All)
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="dropdown-item"
+                    to={"/admin/view-categories-system"}
+                  >
+                    Categories (System)
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="dropdown-item"
+                    to={"/admin/view-categories-user"}
+                  >
+                    Categories (User)
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/admin/add-category"}>
+                Add Categories (System)
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={"/user/budgetset"}>
-                BudgetSet
+              <Link className="nav-link" to={"/admin/transactions"}>
+                View All Transactions
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={"/user/transaction"}>
-                Transaction
+              <Link className="nav-link" to={"/admin/view_user_account"}>
+                View All Users
               </Link>
             </li>
             <li className="nav-item ms-auto">
